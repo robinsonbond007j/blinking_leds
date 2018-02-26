eb3a75b90d257363f48ab336dec72cca8c38fa3f
JOSH\Josh <robinsonbond007j@yahoo.com>
2018-02-25 17:51:04 -0700

    half second flashes

--- led_blinks/led_blinks/led_blinks.ino
+++ led_blinks/led_blinks/led_blinks.ino
@@ -4,12 +4,37 @@
  Author:	Josh
 */
 
+int green = 2;
+int greenState = LOW;
+unsigned long previousMillis = 0;
+long onTime = 500;
+long offTime = 500;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
+	
+	pinMode(green, OUTPUT);
 
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
+
+	unsigned long currentMillis = millis();
+
+	if ((greenState == HIGH) && (currentMillis - previousMillis >= onTime))
+	{
+		greenState = LOW;
+		previousMillis = currentMillis;
+		digitalWrite(green, greenState);
+	}
+
+	else if ((greenState == LOW) && (currentMillis - previousMillis >= offTime))
+	{
+		greenState = HIGH;
+		previousMillis = currentMillis;
+		digitalWrite(green, greenState);
+	}
+
   
 }
