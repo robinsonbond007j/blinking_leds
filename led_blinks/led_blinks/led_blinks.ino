d4557d14642df3cf4d8edbe0fa0b495fcbb3fa22
JOSH\Josh <robinsonbond007j@yahoo.com>
2018-02-25 19:35:07 -0700

    Added yellow and red leds

--- led_blinks/led_blinks/led_blinks.ino
+++ led_blinks/led_blinks/led_blinks.ino
@@ -6,14 +6,32 @@
 
 int green = 2;
 int greenState = LOW;
+
+int yellow = 3;
+int yellowState = LOW;
+
+int red = 4;
+int redState = LOW;
+
+
 unsigned long previousMillis = 0;
-long onTime = 1000;
-long offTime = 1000;
+long onTime = 500;
+long offTime = 500;
+
+unsigned long previousMillis2 = 0;
+long onTime2 = 500;
+long offTime2 = 1000;
+
+unsigned long previousMillis3 = 0;
+long onTime3 = 500;
+long offTime3 = 1500;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
 	
 	pinMode(green, OUTPUT);
+	pinMode(yellow, OUTPUT);
+	pinMode(red, OUTPUT);
 
 }
 
@@ -36,5 +54,32 @@ void loop() {
 		digitalWrite(green, greenState);
 	}
 
+	if ((yellowState == HIGH) && (currentMillis - previousMillis2 >= onTime2))
+	{
+		yellowState = LOW;
+		previousMillis2 = currentMillis;
+		digitalWrite(yellow, yellowState);
+	}
+
+	else if ((yellowState == LOW) && (currentMillis - previousMillis2 >= offTime2))
+	{
+		yellowState = HIGH;
+		previousMillis2 = currentMillis;
+		digitalWrite(yellow, yellowState);
+	}
+
+	if ((redState == HIGH) && (currentMillis - previousMillis3 >= onTime3))
+	{
+		redState = LOW;
+		previousMillis3 = currentMillis;
+		digitalWrite(red, redState);
+	}
+
+	else if ((redState == LOW) && (currentMillis - previousMillis3 >= offTime3))
+	{
+		redState = HIGH;
+		previousMillis3 = currentMillis;
+		digitalWrite(red, redState);
+	}
   
 }
